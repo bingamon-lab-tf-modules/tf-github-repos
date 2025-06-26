b6e1675c9a180466161de7ff509e2c02cec0ba35
MAHDTech <MAHDTech@saltlabs.tech>
2025-06-26 14:46:37 +1000

    chore: add tests (#4)

--- tests/main.tf
+++ tests/main.tf
@@ -1,6 +1,21 @@
 module "test" {
   source = "../module"
 
-  # TF Variables here...
+  github_enterprise_slug   = "acme-corp"
+  github_organization_name = "acme-engineering"
 
+  github_repositories = [
+    {
+      name        = "acme-engineering/acme-service"
+      description = "This is a test repository"
+      visibility  = "public"
+
+      has_issues      = true
+      has_discussions = true
+      has_projects    = true
+      has_wiki        = true
+
+      is_template = false
+    }
+  ]
 }
